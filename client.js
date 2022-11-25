efc9c62f6a536cecc02434a9896cb60be8c306d1
jrivera604 <joshrivera91@hotmail.com>
2022-11-25 21:48:55 +0000

    added stdin.on and ctrl+c

--- client.js
+++ client.js
@@ -3,15 +3,28 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.0.78',
+    host: '10.0.2.15',
     port: '50541',
   });
   conn.on("connect", () => {
+    conn.write('JRR');
+    console.log("Successfully connected to game server");
+    setTimeout(()=> {
+      conn.write('Move: up');
+    }, 500)
+
+    setTimeout(()=> {
+      conn.write('Move: left');
+    }, 500)
+
+    setInterval(() => {
+      conn.write('Move: up');
+    }, 500)
     // code that does something when the connection is first established
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,5 +1,20 @@
 const net = require("net");
 const connect = require('./client')
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  const handleUserInput = function () {
+    if (key === '\u0003') {
+      process.exit();
+    } 
+  };
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 console.log("Connecting ...");
 connect();
