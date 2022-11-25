3add284d4ebb498a36d25cace840b5038beb11d6
jrivera604 <joshrivera91@hotmail.com>
2022-11-25 22:04:21 +0000

    fixed errror with handleUserInPut function

--- play.js
+++ play.js
@@ -6,15 +6,19 @@ const connect = require('./client')
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.on("data", handleUserInput);
-  const handleUserInput = function () {
-    if (key === '\u0003') {
-      process.exit();
-    } 
-  };
+
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
 };
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  } 
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
